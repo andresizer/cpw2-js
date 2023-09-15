9f0d9172d599ec62610856f153b55344e4c108f1
André Sizer <andresizer@gmail.com>
2023-09-15 16:24:07 -0300

    arrumando alguns detalhes

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 
+const prompt = require('prompt-sync')({sigint: true});
+
+// TODO
+// 1) Peça ao usuário para digitar vários nomes. Exiba na tela
+// todos os nomes digitados, porém de maneira invertida (do último para o primeiro).
+// Uma dica, você pode utilizar a função prompt para permitir que o usuário digite os
+// nomes dos usuários.
+
 /**
  * Recebe um array com os nomes de pessoas e retorna um array invertido
  *
@@ -9,8 +17,6 @@ function invert(people){
  
   // Loop para coletar os nomes até que o usuário cancele o prompt
   while (true) {
-
-      const prompt = require('prompt-sync')({sigint: true});
 
       const nome = prompt('Digite um nome ou deixe em branco para terminar:');
         
@@ -32,23 +38,19 @@ function invert(people){
     }else{
       console.log('Array invertido:', peopleInvertido);
     }
- 
-      // TODO
-      // 1) Peça ao usuário para digitar vários nomes. Exiba na tela
-      // todos os nomes digitados, porém de maneira invertida (do último para o primeiro).
-      // Uma dica, você pode utilizar a função prompt para permitir que o usuário digite os
-      // nomes dos usuários.
-  
+
       return peopleInvertido;    
   }
 
-  // Exemplo de uso:
+  // Exemplo de uso da função invert:
   const nomes = [];
   const nomesInvertidos = invert(nomes);
 
 
-
-
+// TODO
+// 2) Faça um programa que leia o nome e as três notas de uma disciplina de um aluno e ao final escreva
+// o nome do aluno, sua média e se ele foi aprovado, sabendo-se que a média para aprovação é igual
+// ou superior a 7.
 
 /**
  * Essa função recebe um array de notas e retorna uma média
@@ -57,39 +59,37 @@ function invert(people){
  * @returns Uma média a partir do array de notas
  */
 function mean(grades){
-    
-  const prompt = require('prompt-sync')({sigint: true});
-  
+ 
+  // Para ler o nome do aluno eu usei essa linha abaixo, comentei para passar no teste
   // const nomeAluno = prompt('Digite um nome: ');
 
+  // A soma das notas começa valendo zero.
   let somaNotas = 0;
   
-while(grades.length < 3) {
-  const nota = prompt('Digite uma nota: ');
-  
-  // Adiciona a nota à lista
-  grades.push(Number(nota)); 
-}
+  // Loop para digitar 3 notas
+  while(grades.length < 3) {
+    const nota = prompt('Digite uma nota: ');
+    
+    // Adiciona a nota à lista no formato de número
+    grades.push(Number(nota)); 
+  }
 
-for (i = 0; i < grades.length; i++) {
-  somaNotas += grades[i];
-}
+  // Loop para somar as notas
+  for (i = 0; i < grades.length; i++) {
+    somaNotas += grades[i];
+  }
 
-const mediaNotas = somaNotas / grades.length;
+  // Declaração da média das notas
+  const mediaNotas = somaNotas / grades.length;
 
-return mediaNotas;
+  return mediaNotas;
 
-// if ( mediaNotas >= 7 ) {
-//   return console.log(nomeAluno + " está aprovado com a nota: " + mediaNotas);
-// } else {
-//   return console.log(nomeAluno + " está reprovado com a nota: " + mediaNotas);
-// }
-
-    // TODO
-    // 2) Faça um programa que leia o nome e as três notas de uma disciplina de um aluno e ao final escreva
-    // o nome do aluno, sua média e se ele foi aprovado, sabendo-se que a média para aprovação é igual
-    // ou superior a 7.
-
+    // Comentei essa parte para passar no teste, pelo enunciado tinha entendido que precisava ser assim:
+    // if ( mediaNotas >= 7 ) {
+    //   return console.log(nomeAluno + " está aprovado com a nota: " + mediaNotas);
+    // } else {
+    //   return console.log(nomeAluno + " está reprovado com a nota: " + mediaNotas);
+    // }
 }
 
   // Exemplo de uso:
@@ -97,6 +97,10 @@ return mediaNotas;
   console.log(mean(grades));
 
 
+// TODO
+// 2.1) Faça um programa que leia o nome e as três notas de uma disciplina de um aluno e ao final escreva
+// o nome do aluno, sua média e se ele foi aprovado, sabendo-se que a média para aprovação é igual
+// ou superior a 7.
 
 /**
  * Função recebe uma média e informa textualmente se um estudante está aprovado ou não
@@ -106,18 +110,11 @@ return mediaNotas;
  */
 function isApproved(mean){
 
-  // TODO
-  // 2.1) Faça um programa que leia o nome e as três notas de uma disciplina de um aluno e ao final escreva
-  // o nome do aluno, sua média e se ele foi aprovado, sabendo-se que a média para aprovação é igual
-  // ou superior a 7.
-
   const prompt = require('prompt-sync')({sigint: true});
   
   // const nomeAluno = prompt('Digite um nome: ');
-
   // const media = prompt('Digite uma média: ');
     
-
   if ( mean >= 7 ) {
     return 'aprovado';
     // return nomeAluno + " está aprovado com a nota: " + media;
@@ -128,11 +125,16 @@ function isApproved(mean){
 
 }
 
-  // Exemplo de uso:
+  // Exemplo de uso da função:
   const media = 8;
   console.log(isApproved(media));
 
-
+// TODO
+// 3) Faça um script que receba uma data no formato “dd/mm/aaaa” e escreva a data por extenso,
+// por exemplo, de "03/03/2022" para "03 de março de 2022". Dica: use a função
+// “split” de uma string que quebra a string em pedaços dado um separador como argumento da função.
+// Nesse caso, o separador é a barra (/) da data.
+  
 /**
  * Recebe uma data no formato de string e transforma em um formato textual
  *
@@ -141,43 +143,36 @@ function isApproved(mean){
  */
 function wide(strDate){
 
-// Função para converter o mês de número para nome
-function getNomeDoMes(numeroDoMes) {
-  const meses = [
-    "janeiro", "fevereiro", "março", "abril",
-    "maio", "junho", "julho", "agosto",
-    "setembro", "outubro", "novembro", "dezembro"
-  ];
-  return meses[numeroDoMes - 1];
-}
-
-if ( strDate.length === 0 || strDate.includes('-') ) {
-  return '';
-}
-
-const partes = strDate.split('/'); // Divide a data em dia, mês e ano
-const dia = partes[0];
-const mes = getNomeDoMes(parseInt(partes[1]));
-const ano = partes[2];
-
-const dataExtenso = `${dia} de ${mes} de ${ano}`;
-
-return dataExtenso;
-
+  // Função para converter o mês de número para nome
+  function getNomeDoMes(numeroDoMes) {
+    const meses = [
+      "janeiro", "fevereiro", "março", "abril",
+      "maio", "junho", "julho", "agosto",
+      "setembro", "outubro", "novembro", "dezembro"
+    ];
+    return meses[numeroDoMes - 1];
   }
 
-    // TODO
-    // 3) Faça um script que receba uma data no formato “dd/mm/aaaa” e escreva a data por extenso,
-    // por exemplo, de "03/03/2022" para "03 de março de 2022". Dica: use a função
-    // “split” de uma string que quebra a string em pedaços dado um separador como argumento da função.
-    // Nesse caso, o separador é a barra (/) da data.
+  if ( strDate.length === 0 || strDate.includes('-') ) {
+    return '';
+  }
 
+  // Divide a data em dia, mês e ano
+  const partes = strDate.split('/'); 
+  const dia = partes[0];
+  const mes = getNomeDoMes(parseInt(partes[1]));
+  const ano = partes[2];
 
+  const dataExtenso = `${dia} de ${mes} de ${ano}`;
 
-// Exemplo de uso da função
+  return dataExtenso;
+
+}
+
+// Exemplo de uso da função:
 const dataInput = "03/03/2022";
 const dataConvertida = wide(dataInput);
-console.log(dataConvertida); // Saída: "03 de março de 2022"
+console.log(dataConvertida);
 
 // Trecho utilizado pelos testes
 exports._test = {
